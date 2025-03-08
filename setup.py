47f530187cb9965360097bbc01f74c4bfc8ef937
mralinp <alinaderiparizi@yahoo.com>
2025-03-08 19:24:49 +0330

    release 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tdsc-abus2023-pytorch",
-    version="0.1.2",
+    version="0.1.3",
     author="Ali Naderi Parizi",
     author_email="me@alinaderiparizi.com",
     description="PyTorch dataset for TDSC ABUS 2023",

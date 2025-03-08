1ea96e52c54fcb2db855a5394c8feb78a9d39a3c
mralinp <alinaderiparizi@yahoo.com>
2025-03-08 18:26:43 +0330

    changed code to src

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="tdsc-abus2023-pytorch",
+    name="tdsc_abus2023_pytorch",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Ali Naderi Parizi",
+    author_email="me@alinaderiparizi.com",
     description="PyTorch dataset for TDSC ABUS 2023",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/tdsc-abus2023-pytorch",
+    url="https://github.com/mralinp/tdsc-abus2023-pytorch",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,5 +23,8 @@ setup(
         "torch>=1.7.0",
         "numpy>=1.19.0",
         "pandas>=1.2.0",
+        "pynrrd>=0.4.2",
+        "gdown>=4.4.0",
+        
     ],
 ) 

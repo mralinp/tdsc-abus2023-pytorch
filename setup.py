6e2975baf6c5a0c60c7b1ab3c42fa4e8aa97673f
mralinp <alinaderiparizi@yahoo.com>
2025-03-08 20:40:58 +0330

    fixed workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tdsc-abus2023-pytorch",
-    version="0.1.4",
+    version="0.1.5",
     author="Ali Naderi Parizi",
     author_email="me@alinaderiparizi.com",
     description="PyTorch dataset for TDSC ABUS 2023",

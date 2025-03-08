8a10f5078fc481c833bc7b78da6b2f86e5ff9657
mralinp <alinaderiparizi@yahoo.com>
2025-03-08 18:02:59 +0330

    configured pypi

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="tdsc-abus2023-pytorch",
-    version="0.1.1",
-    packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "pynrrd",
-        "torch",
-        "requests",
-        "gdown",
-    ],
-    author="Ali Naderi Parizi",
-    author_email="me@alinaderiparizi.com",
-    description="A PyTorch dataset for TDSC ABUS 2023",
-    long_description=open("README.md").read(),
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="PyTorch dataset for TDSC ABUS 2023",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mralinp/tdsc-abus2023-pytorch",
+    url="https://github.com/yourusername/tdsc-abus2023-pytorch",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
+    install_requires=[
+        "torch>=1.7.0",
+        "numpy>=1.19.0",
+        "pandas>=1.2.0",
+    ],
 ) 
